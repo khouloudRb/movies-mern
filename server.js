b0c528b279b67cb244639b495fe0d6f6a244fad8
mairagee524 <garciamaira524@yahoo.com>
2020-12-04 23:01:22 -0800

    added npm packages to server side

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const db = require('./db');
+const movieRouter = require('./routes/api')
+
+const app = express();
+const PORT = process.env.PORT || 3000 ; 
+
+if (app.get('env') === 'development') { require('dotenv').config(); }
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+  
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.get('/', (req, res) => {res.send('Hello World!')});
+
+// Add routes
+app.use('/api', movieRouter);
+
+app.listen(PORT, () => console.log(`🌎 ==> Server running on port ${PORT}`));
