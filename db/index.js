9ccbae1ea64387180ebf4d8f0a6789fffa7c3de9
mairagee524 <garciamaira524@yahoo.com>
2020-12-04 23:38:20 -0800

    changed code to make back-end work

--- db/index.js
+++ db/index.js
@@ -16,28 +16,27 @@ mongoose.connect(
 
 const db = mongoose.connection;
 
-const movieSeed = [
-    {
-      title: "Random Movie",
-      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJ0Ae3DZ67dxAAjCnupYmVbnXSzp6Y18hLDw&usqp=CAU",
-      director: "Dennis Quaid",
-      genre: "Comedy",
-      description: "A puppy is in search of its owner",
-      released: "16 May 2018"
-    }
-];
+// const movieSeed = [
+//     {
+//       title: "Random Movie",
+//       imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJ0Ae3DZ67dxAAjCnupYmVbnXSzp6Y18hLDw&usqp=CAU",
+//       director: "Dennis Quaid",
+//       genre: "Comedy",
+//       description: "A puppy is in search of its owner",
+//       released: "16 May 2018"
+//     }
+// ];
 
-models.Movie
-.remove({})
-.then(() => models.Movie.collection.insertMany(movieSeed))
-.then(data => {
-    console.log(data.result.n + " records inserted!");
-    process.exit(0);
-})
-.catch(err => {
-    console.error(err);
-    process.exit(1);
-});
+// models.Movie
+// .remove({})
+// .then(() => models.Movie.collection.insertMany(movieSeed))
+// .then(data => {
+//     console.log(data.result.n + " records inserted!");
+//     process.exit(0);
+// })
+// .catch(err => {
+//     console.error(err);
+//     process.exit(1);
+// });
   
-
 module.exports = db;
--- server.js
+++ server.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === "production") {
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.get('/', (req, res) => {res.send('Hello World!')});
+// app.get('/', (req, res) => {res.send('Hello World!')});
 
 // Add routes
 app.use('/api', movieRouter);
